7877bae04e0f5ea59471dfbd28026386ece9c910
officialYash <38553961+officialYash@users.noreply.github.com>
2023-03-06 00:01:01 +0530

    Hero Section and who content made it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,10 +19,65 @@ display:flex;
 justify-content:space-between;
 `
 const Left= styled.div`
-
+flex:2;
+display:flex;
+flex-direction:column;
+gap:20px;
+justify-content:center;
 `
+const Title=styled.h1`
+font-size:74px;
+`;
+const WhatWeDo=styled.div`display:flex;
+align-items:center;
+gap:10px;`;
+
+const Line=styled.img`
+ height:5px;`;
+
+const Subtitle=styled.h2`
+color:#da4ea2;  
+`;
+const Desc=styled.p`
+font-size:24px;
+color: lightgray;
+`;
+const Button= styled.button`
+background-color:#da4ea2;
+color:white;
+font-weight:500;
+width: 100px;
+padding: 10px;
+border:none;
+border-radius:5px;
+cursor:pointer;
+`;
+const Img=styled.img`
+width:800px;
+height:600px;
+object-fill: contain;
+position: absolute;
+top:0;
+bottom:0;
+left:0;
+right:0;
+margin:auto;
+animation: animate 2s infinite ease alternate;
+
+@keyframes animate {
+to {
+transform: translateY(20px);
+}
+}
+`;
+
+
+
+
 
 const Right= styled.div`
+flex:3;
+position:relative;
 `
 
 
@@ -33,7 +88,7 @@ const Hero = () => {
     <Container>
     <Left>
       <Title>
-        Think , Make .Solve
+        Think. Make. Solve.</Title>
         <WhatWeDo>
         <Line src="./img/line.png"/>
           <Subtitle>
@@ -42,7 +97,6 @@ const Hero = () => {
         </WhatWeDo>
         <Desc>we enjoy creating delightful , Human-centered digital experiences</Desc>
       <Button>Learn More</Button>
-      </Title>
     </Left>
     <Right>
     {/* 3D Model  */}
--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,16 +1,82 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 const Section = styled.div`
 height: 100vh;
 scroll-snap-align: center;
-
-
+display:flex;
+justify-content: center;
 `
+const Container= styled.div`
+height:100vh;
+scroll-snap-align:center;
+width: 1400px;
+display:flex;
+justify-content:space-between;
+`
+const Left= styled.div`
+flex:1;
+`
+const Title=styled.h1`
+font-size:74px;
+`;
+const WhatWeDo=styled.div`display:flex;
+align-items:center;
+gap:10px;`;
+
+const Line=styled.img`
+ height:5px;`;
+
+const Subtitle=styled.h2`
+color:#da4ea2;  
+`;
+const Desc=styled.p`
+font-size:24px;
+color: lightgray;
+`;
+const Button= styled.button`
+background-color:#da4ea2;
+color:white;
+font-weight:500;
+width: 120px;
+padding: 10px;
+border:none;
+border-radius:5px;
+cursor:pointer;
+`;
+
+const Right= styled.div`
+flex:1;
+display:flex;
+flex-direction:column;
+gap:20px;
+justify-content:center;
+`
+
 
 const Who = () => {
   return (
-    <Section>whos</Section>
+    <Section>
+    <Container>
+    <Left>
+  {/* 3D Model  */}
+    </Left>
+    <Right>
+    <Title>
+        Think Outside the Square space</Title>
+        <WhatWeDo>
+        <Line src="./img/line.png"/>
+          <Subtitle>
+            Who We are
+          </Subtitle>
+        </WhatWeDo>
+        <Desc>a creative group of designers and developers with passion for the arts. </Desc>
+      <Button>See Our works</Button>
+    </Right>
+    </Container>
+
+    </Section>
   )
 }
 
