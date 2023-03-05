cbeeeb61d2ba27baa68743596fce75d413e620fa
officialYash <38553961+officialYash@users.noreply.github.com>
2023-03-06 01:42:12 +0530

    Contact US content made it

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,77 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div`
+width:   100%;
+height: 100%;
+display:flex;
+justify-content: space-between;
+gap: 50px;
+`
 
 const Section = styled.div`
 height: 100vh;
 scroll-snap-align: center;
-
+`
+const Title = styled.h1`
+font-weight:200;
+`
+const Left = styled.div`
+flex:1;
+display:flex;
+align-item:center;
+justify-content:flex-end;
+`
+const Right = styled.div`
+flex:1;
 
 `
+const Input = styled.input`
+padding:20px;
+background-color: #e8e6e6;
+border:none;
+border-radius:5px;
+`
+const TextArea = styled.textarea`
+padding:20px;
+border:none;
+border-radius:5px;
+background-color: #e8e6e6;
+
+`
+const Button = styled.button`
+background-color:#da4ea2;
+color:white;
+border:none;
+font-weight: bold;
+cursor:pointer;
+border-radius:5px;
+padding:20px;
+`
+const Form = styled.form`
+width:500px;
+display:flex;
+flex-direction:column;
+gap:25px;
+`
+
 const Contact = () => {
   return (
-    <Section>Contact</Section>
+    <Section>
+    <Container>
+      <Left>
+        <Form>
+          <Title>Contact Us</Title>
+          <Input placeholder='Name'/>
+          <Input placeholder='Email'/>
+          <TextArea placeholder='Write your message' rows={10}/>
+          <Button>Send</Button>
+        </Form>
+      </Left>
+      <Right></Right>
+    </Container>
+
+    </Section>
   )
 }
 
