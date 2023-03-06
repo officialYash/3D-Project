b8b68f4e77c0a6764981884debd33f13debae39c
officialYash <38553961+officialYash@users.noreply.github.com>
2023-03-06 18:23:15 +0530

    graphic 3d Image have a set on different section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components"
 import Contact from "./components/Contact"
 import Hero from "./components/Hero"
-import Test from "./components/Test"
 import Who from "./components/Who"
 import Works from "./components/Works"
 
@@ -26,7 +25,6 @@ function App() {
     <Who/>
     <Works/>
     <Contact/>
-    <Test/>
     </Container>
   )
 }
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-
+import Development from "./Development"
+import ProductDesugn from "./ProductDesugn"
+import WebDesign from "./WebDesign"
 
 const data=[
   "Web Design",
@@ -72,17 +74,20 @@ flex:1
 
 
 const Works = () => {
+  const [work , setWork] = useState("Web Design")
   return (
     <Section>
     <Container>
       <Left>
       <List>
       {data.map((item) => (
-        <ListItem key={item} text={item}>{item}</ListItem>
+        <ListItem key={item} text={item} onClick={()=> setWork(item)}>{item}</ListItem>
       ))}
       </List>
      </Left>
-    <Right></Right>
+    <Right>
+      {work === "Web Design" ? (<WebDesign/>) : work === "Development" ? (<Development/>) : (<ProductDesugn/> )}
+    </Right>
     </Container>
     </Section>
   )
